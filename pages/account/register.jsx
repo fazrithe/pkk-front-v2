fd1824753c5a08cd87b82316b7a2d9c338a38029
MOH DEDE FAZRI <fazrithe@gmail.com>
2023-01-19 09:40:56 +0700

    first commit
    first commit

---
+++ pages/account/register.jsx
@@ -0,0 +1,86 @@
+import { useRouter } from 'next/router';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+
+import { Link } from 'components';
+import { Layout } from 'components/account';
+import { userService, alertService } from 'services';
+
+export default Register;
+
+function Register() {
+    const router = useRouter();
+
+    // form validation rules 
+    const validationSchema = Yup.object().shape({
+        name: Yup.string()
+            .required('Name is required'),
+        email: Yup.string()
+            .required('Email is required'),
+        username: Yup.string()
+            .required('Username is required'),
+        password: Yup.string()
+            .required('Password is required')
+            .min(6, 'Password must be at least 6 characters'),
+        confPassword: Yup.string()
+            .required('Confirm Password is required')
+            .min(6, 'Confirm Password must be at least 6 characters')
+    });
+    const formOptions = { resolver: yupResolver(validationSchema) };
+
+    // get functions to build form with useForm() hook
+    const { register, handleSubmit, formState } = useForm(formOptions);
+    const { errors } = formState;
+
+    function onSubmit(user) {
+        return userService.register(user)
+            .then(() => {
+                alertService.success('Registration successful', { keepAfterRouteChange: true });
+                router.push('login');
+            })
+            .catch(alertService.error);
+    }
+
+    return (
+        <Layout>
+            <div className="card">
+                <h4 className="card-header">Register</h4>
+                <div className="card-body">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="form-group">
+                            <label>Name</label>
+                            <input name="name" type="text" {...register('name')} className={`form-control ${errors.name ? 'is-invalid' : ''}`} />
+                            <div className="invalid-feedback">{errors.name?.message}</div>
+                        </div>
+                        <div className="form-group">
+                            <label>Email</label>
+                            <input name="email" type="text" {...register('email')} className={`form-control ${errors.email ? 'is-invalid' : ''}`} />
+                            <div className="invalid-feedback">{errors.email?.message}</div>
+                        </div>
+                        <div className="form-group">
+                            <label>Username</label>
+                            <input name="username" type="text" {...register('username')} className={`form-control ${errors.username ? 'is-invalid' : ''}`} />
+                            <div className="invalid-feedback">{errors.username?.message}</div>
+                        </div>
+                        <div className="form-group">
+                            <label>Password</label>
+                            <input name="password" type="password" {...register('password')} className={`form-control ${errors.password ? 'is-invalid' : ''}`} />
+                            <div className="invalid-feedback">{errors.password?.message}</div>
+                        </div>
+                        <div className="form-group">
+                            <label>Password</label>
+                            <input name="confPassword" type="password" {...register('confPassword')} className={`form-control ${errors.confPassword ? 'is-invalid' : ''}`} />
+                            <div className="invalid-feedback">{errors.confPassword?.message}</div>
+                        </div>
+                        <button disabled={formState.isSubmitting} className="btn btn-primary">
+                            {formState.isSubmitting && <span className="spinner-border spinner-border-sm mr-1"></span>}
+                            Register
+                        </button>
+                        <Link href="/account/login" className="btn btn-link">Cancel</Link>
+                    </form>
+                </div>
+            </div>
+        </Layout>
+    );
+}
